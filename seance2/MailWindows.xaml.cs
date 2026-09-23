c54c0160b03d9768f224d78c0ccd3da593c8f9f6
Mariam <mariam@gmail.com>
2026-09-23 12:50:07 +0300

    Envoi de mail avec System.Net.Mail

--- seance2/MailWindows.xaml.cs
+++ seance2/MailWindows.xaml.cs
@@ -11,7 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using System.Net;
+using System.Net.Mail;
+using System.Linq.Expressions;
 namespace seance2
 {
     /// <summary>
@@ -24,9 +26,28 @@ namespace seance2
             InitializeComponent();
         }
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        private void btnEnvoyer_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(txtEmail.Text);
+                mail.To.Add(txtDestinataire.Text);
+                mail.Subject = txtObjet.Text;
+                mail.Body = txtMessage.Text;
 
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.EnableSsl = true;
+                smtp.Credentials = new NetworkCredential(txtEmail.Text, pwdPassword.Password);
+                smtp.Send(mail);
+                MessageBox.Show("Mail envoye!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur : "+ ex.Message, "Erreur : ", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
         }
+        
     }
 }
